3a18b4ad9b65b3539e452dc0cac653a4bcf11ec3
Ger Hobbelt <ger@hobbelt.com>
2025-08-11 08:52:36 +0200

    fix compiler warning about missing prototype of void function.

--- test/subtest.cpp
+++ test/subtest.cpp
@@ -2,7 +2,7 @@
 #include "termcolor/termcolor.hpp"
 
 
-void subtest_from_another_tranlation_unit()
+void subtest_from_another_tranlation_unit(void)
 {
     std::cout << termcolor::blue << "subtest" << termcolor::reset << std::endl;
 }
--- test/subtest.hpp
+++ test/subtest.hpp
@@ -1,6 +1,6 @@
 #ifndef SUBTEST_HPP_
 #define SUBTEST_HPP_
 
-void subtest_from_another_tranlation_unit();
+void subtest_from_another_tranlation_unit(void);
 
 #endif // SUBTEST_HPP_
